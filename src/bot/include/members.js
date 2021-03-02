6efb3d396f96048906abbde722bd208f5560efd4
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-02 12:02:58 -0300

    fix env

--- src/bot/include/members.js
+++ src/bot/include/members.js
@@ -1,6 +1,6 @@
 module.exports = {
     async members(guild) {
-        const channel = await guild.channel.cache.get(process.env.members);
+        const channel = await guild.channel.cache.get(process.env.MEMBERS);
         channel.setName(`${guild.memberCount} membros`);
     }
 }

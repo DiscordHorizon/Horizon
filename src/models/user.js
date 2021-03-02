f0cc5108fe0f8da808a8d427433ae54f362179d7
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-02 17:14:34 -0300

    user model

---
+++ src/models/user.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require('mongoose');
+
+const User = Schema({
+    id: String,
+    lastConnection: Number,
+    accumulatedTime: Number,
+    tasks: Array
+});
+
+module.exports = model("User", User);

1123a7d670c7d8f76d95e4a342c9c4576e0873b4
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-02 11:27:15 -0300

    add presence

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,6 +1,18 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+let presence = false;
+
+async function botPresence() {
+    presence = !presence;
+    if(presence === false) {
+        await bot.user.setPresence({ activity: { name: `Precisa de ajuda? mande "help" no privado.`, type: 1, url: 'https://twitch.tv/bravanzin' }});
+    } else {
+        await bot.user.setPresence({ activity: { name: `Seja bem vindo ao Horizon, ${bot.users.cache.size} presentes no servidor!`, type: 1, url: 'https://twitch.tv/bravanzin' }});
+    }
+}
+setInterval(botPresence, 7000);
+
 bot.on('ready', () => {
     console.log('[Bot] Connected');
 })

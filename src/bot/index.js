09eb5e5a34084a2700aa5ff946b36bc719ffcae1
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-02 11:58:38 -0300

    add member count

--- src/bot/index.js
+++ src/bot/index.js
@@ -1,20 +1,33 @@
 const Discord = require('discord.js');
+const { members } = require('./include/members');
+
 const bot = new Discord.Client();
 
+//* dynamic presence
 let presence = false;
-
 async function botPresence() {
     presence = !presence;
     if(presence === false) {
-        await bot.user.setPresence({ activity: { name: `Precisa de ajuda? mande "help" no privado.`, type: 1, url: 'https://twitch.tv/bravanzin' }});
+        await bot.user.setPresence({ activity: { name: `${bot.users.cache.size} membros no servidor.`, type: 1, url: 'https://twitch.tv/bravanzin' }});
     } else {
-        await bot.user.setPresence({ activity: { name: `Seja bem vindo ao Horizon, ${bot.users.cache.size} presentes no servidor!`, type: 1, url: 'https://twitch.tv/bravanzin' }});
+        await bot.user.setPresence({ activity: { name: `Seja bem vindo ao Horizon`, type: 1, url: 'https://twitch.tv/bravanzin' }});
     }
 }
 setInterval(botPresence, 7000);
 
+//* bot ready
 bot.on('ready', () => {
+    await bot.user.setPresence({ activity: { name: `Seja bem vindo ao Horizon!!`, type: 1, url: 'https://twitch.tv/bravanzin' }});
     console.log('[Bot] Connected');
+});
+
+//* member count update
+bot.on('guildMemberAdd', user => {
+    members(user.guild);
+});
+
+bot.on('guildMemberRemove', user => {
+    members(user.guild);
 })
 
 bot.login(process.env.DISCORD_TOKEN);

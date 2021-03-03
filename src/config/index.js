7bddc2bf18489706fa2fa1f01736c210cd0a4e1a
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-03 11:03:30 -0300

    setup config vars

---
+++ src/config/index.js
@@ -0,0 +1,25 @@
+let config;
+try {
+    config = require('../../config.json');
+} catch (error) {
+    config = null;
+};
+
+exports.config = {
+    tokens: {
+        discord: config ? config.discord : process.env.DISCORD_TOKEN,
+        mongoUri: config ? config.mongo : process.env.MONGO_URI
+    },
+    channels: {
+        guild: config ? config.channels.guild : process.env.GUILD,
+        users: config ? config.channels.users : process.env.USERS,
+        welcome: config ? config.channels.welcome : process.env.WELCOME,
+        rules: config ? config.channels.rules : process.env.RULES,
+        tasks: {
+            log: config ? config.channels.tasks.log : process.env.LOG,
+            tasks: config ? config.channels.tasks.tasks : process.env.TASKS,
+            add: config ? config.channels.tasks.add : process.env.ADD,
+            remove: config ? config.channels.tasks.remove : process.env.REMOVE,
+        }
+    }
+}

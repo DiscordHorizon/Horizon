a1b3465f7c4d97f5e6d410ca4137868c0e9122f7
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-03 19:38:49 -0300

    create voiceRole function

--- src/include/voice.js
+++ src/include/voice.js
@@ -1,4 +1,21 @@
 module.exports = {
-    async voiceRole(state) {
+    async voiceRole(oldState, newstate) {
+        const userId = newState.id;
+        const oldChannel = oldState.channelID
+        const newChannel = newState.channelID;
+
+        if(newChannel) {
+            const channel = newState.guild.channels.cache.get(newChannel);
+            const voiceRole = newChannel.guild.roles.cache.find(
+                roles => roles.name === channel.name
+            );
+            state.guild.members.cache.get(userId).roles.add(voiceRole);
+        } else {
+            const channel = oldState.guild.channels.cache.get(oldChannel);
+            const voiceRole = oldState.guild.roles.cache.find(
+                roles => roles.name === channel.name
+            );
+            state.guild.members.cache.get(userId).roles.add(voiceRole);
+        }
     }
 }

41443838f538fc78a6ee563442fc663455b81e0c
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-02 23:37:15 -0300

    create msg template

--- src/include/message.js
+++ src/include/message.js
@@ -3,6 +3,6 @@ const { MessageEmbed } = require('discord.js');
 const message = new MessageEmbed()
     .setColor("3498DB")
     .setTimestamp(new Date())
-    .setFooter("Exphare - Aprenda mais!", "https://cdn.discordapp.com/app-icons/688571869275881503/b5bfeb52ddae6f9492925772a59e1f8d.png?size=512");
+    .setFooter("Horizon", "https://cdn.discordapp.com/app-icons/688571869275881503/b5bfeb52ddae6f9492925772a59e1f8d.png?size=512");
 
 module.exports = message;

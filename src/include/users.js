c881529ebf335c35f414219d1eb0451eb605ca2f
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-03 22:14:26 -0300

    update text

--- src/include/users.js
+++ src/include/users.js
@@ -3,6 +3,6 @@ const { config } = require('../config');
 module.exports = {
     async users(guild) {
         const channel = await guild.channels.cache.get(config.channels.users);
-        channel.setName(`${guild.memberCount} membros`);
+        channel.setName(`Horizon Members: ${guild.memberCount}`);
     }
 }

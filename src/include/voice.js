385cb2e6462eca42be9e762c8c6c68da4de8e4e6
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-03 23:53:19 -0300

    fix roles

--- src/include/voice.js
+++ src/include/voice.js
@@ -1,9 +1,10 @@
-const userModel = require('../models/user');
-const { config } = require('../config');
+const userModel = require("../models/user");
+const { config } = require("../config");
 
 module.exports = {
     async voiceRole(oldState, newState) {
-        //* user id
+        //* user
+        const req = await userModel.findOne({ id: oldState.id });
         const user = newState.guild.members.cache.get(newState.id);
 
         //* channels
@@ -12,58 +13,39 @@ module.exports = {
 
         //* roles
         const horizon = newState.guild.roles.cache.find(
-            roles => roles.name === "Horizon Member"
+            (roles) => roles.name === "Horizon Member"
         );
         const valorant = newState.guild.roles.cache.find(
-            roles => roles.name === "Valorant"
+            (roles) => roles.name === "Valorant"
         );
         const leagueoflegends = newState.guild.roles.cache.find(
-            roles => roles.name === "League of Legends"
+            (roles) => roles.name === "League of Legends"
         );
         const ragnarok = newState.guild.roles.cache.find(
-            roles => roles.name === "Ragnarok"
+            (roles) => roles.name === "Ragnarok"
         );
-        
 
-        if(newChannel) {
+        if (newChannel) {
             const channel = newState.guild.channels.cache.get(newChannel);
-            
+
             //* add voice channel role
             const voiceRole = newState.guild.roles.cache.find(
-                roles => roles.name === channel.name
+                (roles) => roles.name === channel.name
             );
             user.roles.add(voiceRole);
 
-            // * remove Horizon Member role
+            // * remove roles
             user.roles.remove(horizon);
-
-            //* game role
-            
-            //* valorant
-            if (channel.parentID === config.categorys.valorant) {
-                user.roles.remove(leagueoflegends);
-                user.roles.remove(ragnarok);
-            };
-
-            //* league of legends
-            if (channel.parentID === config.categorys.leagueoflegends) {
-                user.roles.remove(valorant);
-                user.roles.remove(ragnarok);
-            };
-
-            //* ragnarok
-            if (channel.parentID === config.categorys.ragnarok) {
-                user.roles.remove(valorant);
-                user.roles.remove(leagueoflegends);
-            };
+            user.roles.remove(valorant);
+            user.roles.remove(leagueoflegends);
+            user.roles.remove(ragnarok);
 
         } else {
-            const req = await userModel.findOne({ id: oldState.id });
             const channel = oldState.guild.channels.cache.get(oldChannel);
-            
+
             //* remove voice channel role
             const voiceRole = oldState.guild.roles.cache.find(
-                roles => roles.name === channel.name
+                (roles) => roles.name === channel.name
             );
             user.roles.remove(voiceRole);
 
@@ -71,12 +53,12 @@ module.exports = {
             user.roles.add(horizon);
 
             //* game role
-            req.games.forEach(game => {
+            req.games.forEach((game) => {
                 const role = oldState.guild.roles.cache.find(
-                    roles => roles.name === game
+                    (roles) => roles.name === game
                 );
                 user.roles.add(role);
             });
         }
-    }
-}
+    },
+};

0924c9b6e90e24557be8121f7a2fedb7d6245db1
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-04 15:30:28 -0300

    fix change channel role

--- src/include/voice.js
+++ src/include/voice.js
@@ -1,32 +1,17 @@
-const userModel = require("../models/user");
-const { config } = require("../config");
-
 module.exports = {
     async voiceRole(oldState, newState) {
         //* user
-        const req = await userModel.findOne({ id: oldState.id });
         const user = newState.guild.members.cache.get(newState.id);
 
         //* channels
         const oldChannel = oldState.channelID;
         const newChannel = newState.channelID;
 
-        //* roles
-        const horizon = newState.guild.roles.cache.find(
-            (roles) => roles.name === "Horizon Member"
-        );
-        const valorant = newState.guild.roles.cache.find(
-            (roles) => roles.name === "Valorant"
-        );
-        const leagueoflegends = newState.guild.roles.cache.find(
-            (roles) => roles.name === "League of Legends"
-        );
-        const ragnarok = newState.guild.roles.cache.find(
-            (roles) => roles.name === "Ragnarok"
-        );
-
         if (newChannel) {
             const channel = newState.guild.channels.cache.get(newChannel);
+            const lastChannel = oldState.guild.channels.channelID.get(
+                oldChannel
+            );
 
             //* add voice channel role
             const voiceRole = newState.guild.roles.cache.find(
@@ -34,11 +19,13 @@ module.exports = {
             );
             user.roles.add(voiceRole);
 
-            // * remove roles
-            user.roles.remove(horizon);
-            user.roles.remove(valorant);
-            user.roles.remove(leagueoflegends);
-            user.roles.remove(ragnarok);
+            //* remove last voice channel role
+            if (oldChannel) {
+                const lastVoiceRole = newState.guild.roles.cache.find(
+                    (roles) => roles.name === lastChannel.name
+                );
+                user.roles.remove(lastVoiceRole);
+            }
 
         } else {
             const channel = oldState.guild.channels.cache.get(oldChannel);
@@ -48,17 +35,6 @@ module.exports = {
                 (roles) => roles.name === channel.name
             );
             user.roles.remove(voiceRole);
-
-            // * add Horizon Member role
-            user.roles.add(horizon);
-
-            //* game role
-            req.games.forEach((game) => {
-                const role = oldState.guild.roles.cache.find(
-                    (roles) => roles.name === game
-                );
-                user.roles.add(role);
-            });
         }
     },
 };

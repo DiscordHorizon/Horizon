901f08f2c2ee1e8d51ffb36e73ac3d33ee27654f
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-06 15:49:17 -0300

    remove old level role

--- src/include/level.js
+++ src/include/level.js
@@ -4,6 +4,13 @@ module.exports = {
     async setLevel(user, roles) {
         const req = await userModel.findOne({ id: user.id });
 
+        user._roles.forEach(roleId => {
+            const role = roles.find(roles => roles.id === roleId)
+            if (role.name.startsWith('level')) {
+                user.roles.remove(role);
+            }
+        });
+
         const level = roles.find( roles => roles.name === `level ${req.level}` );
 
         user.roles.add(level);

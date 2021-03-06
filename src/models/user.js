35bd33424825bbf988fc0c38c40847f8dccfa103
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-06 11:10:54 -0300

    add level

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const User = Schema({
     id: String,
+    level: Number,
     lastConnection: Number,
     accumulatedTime: Number,
     tasks: Array,

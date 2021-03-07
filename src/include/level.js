579d5141d69c6cf7a509d77135a32faa32cc4d30
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-06 21:48:12 -0300

    finish updateHorizonRole function

--- src/include/level.js
+++ src/include/level.js
@@ -1,24 +1,27 @@
 const userModel = require("../models/user");
 const { config } = require("../config");
 
-async function removeHorizonRoles(user, roles, ) {
-    config.rolesArray.forEach((roleConfig) => {
-        user._roles.forEach((roleId) => {
-            const role = roles.find((roles) => roles.id === roleId);
-            if (role.id === roleConfig) {
-                user.roles.remove(role);
-            }
-        });
-    });
-};
-
-async function addHorizonRole(user, userRoles, ) {
+async function updateHorizonRole(user, guildRoles) {
+    const userReq = await userModel.findOne({ id: user.id });
     const roles = config.roles;
-    var roleLevel;
+    var adding = true;
 
-    roles.forEach(role => {
-        if (user.level >= role.level) {
-            roleLevel = role;
+    roles.forEach((role) => {
+        if (userReq.level >= role.level) {
+            if (adding) {
+                const horizonRole = guildRoles.find(
+                    (roles) => roles.id === role.id
+                );
+                user.roles.add(horizonRole);
+                adding = false;
+            } else {
+                user._roles.forEach((roleId) => {
+                    const userRole = roles.find((roles) => roles.id === roleId);
+                    if (userRole.id === role.id) {
+                        user.roles.remove(role);
+                    }
+                });
+            }
         }
     });
 }
@@ -40,114 +43,7 @@ module.exports = {
         );
         user.roles.add(level);
 
-        //* update horizon role
-        addHorizonRole(req, roles);
-
-        /*
-        //* heaven
-        if (req.level === 99) {
-            const role = roles.find(
-                (roles) => roles.id === config.rolesIds.heaven
-            );
-            user.roles.add(role);
-        }
-        //* clouds
-        if (req.level > 95 && req.level < 99) {
-            const role = roles.find(
-                (roles) => roles.id === config.rolesIds.clouds
-            );
-            user.roles.add(role);
-        }
-        //* snow
-        if (req.level > 90 && req.level < 96) {
-            const role = roles.find(
-                (roles) => roles.id === config.rolesIds.snow
-            );
-            user.roles.add(role);
-        }
-        //* ice peaks
-        if (req.level > 85 && req.level < 91) {
-            const role = roles.find(
-                (roles) => roles.id === config.rolesIds.icePeaks
-            );
-            user.roles.add(role);
-        }
-        //* emerald fishs
-        if (req.level > 80 && req.level < 86) {
-            const role = roles.find(
-                (roles) => roles.id === config.rolesIds.fishs
-            );
-            user.roles.add(role);
-        }
-        //* seas
-        if (req.level > 75 && req.level < 81) {
-            const role = roles.find(
-                (roles) => roles.id === config.rolesIds.seas
-            );
-            user.roles.add(role);
-        }
-        //* lights
-        if (req.level > 70 && req.level < 76) {
-            const role = roles.find(
-                (roles) => roles.id === config.rolesIds.lights
-            );
-            user.roles.add(role);
-        }
-        //* rainbows
-        if (req.level > 65 && req.level < 71) {
-            const role = roles.find(
-                (roles) => roles.id === config.rolesIds.rainbows
-            );
-            user.roles.add(role);
-        }
-        //* vulcans
-        if (req.level > 55 && req.level < 66) {
-            const role = roles.find(
-                (roles) => roles.id === config.rolesIds.vulcans
-            );
-            user.roles.add(role);
-        }
-        //* shiny stones
-        if (req.level > 45 && req.level < 56) {
-            const role = roles.find(
-                (roles) => roles.id === config.rolesIds.shinyStones
-            );
-            user.roles.add(role);
-        }
-        //* stones
-        if (req.level > 35 && req.level < 46) {
-            const role = roles.find(
-                (roles) => roles.id === config.rolesIds.stones
-            );
-            user.roles.add(role);
-        }
-        //* rivers
-        if (req.level > 25 && req.level < 36) {
-            const role = roles.find(
-                (roles) => roles.id === config.rolesIds.rivers
-            );
-            user.roles.add(role);
-        }
-        //* flowers
-        if (req.level > 15 && req.level < 26) {
-            const role = roles.find(
-                (roles) => roles.id === config.rolesIds.flowers
-            );
-            user.roles.add(role);
-        }
-        //* trees
-        if (req.level > 5 && req.level < 16) {
-            const role = roles.find(
-                (roles) => roles.id === config.rolesIds.trees
-            );
-            user.roles.add(role);
-        }
-        //* arriving
-        if (req.level < 6) {
-            const role = roles.find(
-                (roles) => roles.id === config.rolesIds.arriving
-            );
-            user.roles.add(role);
-        }*/
+        //* update horizon roles
+        updateHorizonRole(user, roles);
     },
 };
